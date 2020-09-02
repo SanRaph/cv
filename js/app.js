12ee476c1e2093a35bd3a4347f7a1b88f94268ac
San Raph <raphytex@gmail.com>
2020-09-02 17:28:41 +0100

    add initial cv website files

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const icon = document.getElementsByClassName('fas fa-chevron-right');
+const input = document.getElementsByTagName('input');
+if(input.type !== null){
+icon.textContent = 'Submit';
+}
